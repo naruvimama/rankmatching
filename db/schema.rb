c4d9e9be52be4d13757f1becca77801a927d0dea
chandra <naruvimama@gmail.com>
2016-11-30 23:06:06 +0100

    Rename user to owner in projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130204538) do
+ActiveRecord::Schema.define(version: 20161130204908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20161130204538) do
     t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "owner_id"
   end
 
   create_table "users", force: true do |t|
