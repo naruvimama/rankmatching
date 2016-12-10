b48be99924999c0336ca43736378ac66ad516cd9
chandra <naruvimama@gmail.com>
2016-12-10 22:09:56 +0100

    Fix project model, add Project routes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
 	has_one :owner, inverse_of: :owned_projects, class_name: :User
-	has_dag_links link_class_name: :DAGLink
+	has_dag_links link_class_name: "DagLink"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'landings/index'
 
+  get 'landings/index'
+  
   resources :profiles
+  resources :projects
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
