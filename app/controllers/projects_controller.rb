e483c233a2e31c1f9889ff0e7a9ee31bd6523108
chandra <naruvimama@gmail.com>
2016-12-10 22:11:09 +0100

    Add project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,21 @@
+class ProjectsController < ApplicationController
+  load_resource
+  
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
