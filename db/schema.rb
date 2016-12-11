5e0558bbca504a63943e81fb389240ff0c29af48
chandra <naruvimama@gmail.com>
2016-12-11 11:43:23 +0100

    Add website to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210200118) do
+ActiveRecord::Schema.define(version: 20161211104007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20161210200118) do
     t.datetime "updated_at"
     t.integer  "owner_id"
     t.boolean  "is_ghost"
+    t.string   "website"
   end
 
   create_table "taggings", force: true do |t|
