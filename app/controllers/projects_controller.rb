1458374d4e22a2ed7cb82880fc8cefcc51df6d48
chandra <naruvimama@gmail.com>
2016-12-11 11:57:45 +0100

    Add project params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,4 +18,13 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def project_params
+      params.require(:project).permit([:name, :description, :ghost_id, :logo, :picture, :is_ghost])
+    end
+
+    def owner_params
+      project_params.permit [:owner_id]
+    end
 end
